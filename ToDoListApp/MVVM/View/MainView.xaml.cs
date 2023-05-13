d2a75a005607a9213df231247870c4edb707a430
julita.gajewska <j.gajewska.4@gmail.com>
2023-05-13 20:26:54 +0200

    Login view - 90% done, closing login view and opening a main view needs to be fixed

--- ToDoListApp/MVVM/View/MainView.xaml.cs
+++ ToDoListApp/MVVM/View/MainView.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ToDoListApp
+namespace ToDoListApp.MVVM.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : Window
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
---
+++ ToDoListApp/Repositiories/RepositoryBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Data.SqlClient;
+
+namespace ToDoListApp.Repositiories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+
+        public RepositoryBase()
+        {
+            _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ToDoDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection( _connectionString );
+        }
+    }
+}
