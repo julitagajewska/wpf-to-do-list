f396eba2cb13fe47643c2edc9aec67626ca4f7fd
Aleksandra Niedźwiecka <olaniedz00@wp.pl>
2023-06-01 14:06:50 +0200

    EditCategory

--- ToDoListApp/MVVM/ViewModel/CategoryPanelViewModel.cs
+++ ToDoListApp/MVVM/ViewModel/CategoryPanelViewModel.cs
@@ -1,70 +1,95 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Input;
-using ToDoListApp.Data;
-using ToDoListApp.MVVM.Model;
-using ToDoListApp.MVVM.Model.Interfaces;
-using ToDoListApp.MVVM.Model.Services;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+﻿    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using ToDoListApp.Data;
+    using ToDoListApp.MVVM.Model;
+    using ToDoListApp.MVVM.Model.Interfaces;
+    using ToDoListApp.MVVM.Model.Services;
+    using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
-namespace ToDoListApp.MVVM.ViewModel
-{
-    public class CategoryPanelViewModel : ViewModelBase
+    namespace ToDoListApp.MVVM.ViewModel
     {
-        private readonly ToDoDbContext _context;
-        private readonly IUserRepository _userRepository;
-        private readonly ICategoryRepository _categoryRepository;
-        private UserModel _loggedInUser;
-
-        private ObservableCollection<Category> _userCategories;
-        public ObservableCollection<Category> UserCategories
+        public class CategoryPanelViewModel : ViewModelBase
         {
-            get { return _userCategories; }
-            set
+            private readonly ToDoDbContext _context;
+            private readonly IUserRepository _userRepository;
+            private readonly ICategoryRepository _categoryRepository;
+            private UserModel _loggedInUser;
+
+            private ObservableCollection<Category> _userCategories;
+            public ObservableCollection<Category> UserCategories
             {
-                _userCategories = value;
-                OnPropertyChanged(nameof(UserCategories));
+                get { return _userCategories; }
+                set
+                {
+                    _userCategories = value;
+                    OnPropertyChanged(nameof(UserCategories));
+                }
             }
-        }
-
-        public ICommand EditCategoryCommand { get; set; }
-        public ICommand DeleteCategoryCommand { get; set; }
-        public CategoryPanelViewModel(UserModel loggedInUser)
-        {
-            _context = new ToDoDbContext();
-            _loggedInUser = loggedInUser;
-            _userRepository = new UserRepository(_context);
-            _categoryRepository = new CategoryRepository(_context);
-            EditCategoryCommand = new ViewModelCommand(ExecuteEditCategoryCommand);
-            DeleteCategoryCommand = new ViewModelCommand(ExecuteDeleteCategoryCommand);
+            private Category _selectedCategory;
+            public Category SelectedCategory
+            {
+                get { return _selectedCategory; }
+                set
+                {
+                    _selectedCategory = value;
+                    OnPropertyChanged(nameof(SelectedCategory));
+                }
+            }
+            public ICommand EditCategoryCommand { get; set; }
+            public ICommand DeleteCategoryCommand { get; set; }
+            public ICommand SaveCategoryCommand { get; set; }
+            public CategoryPanelViewModel(UserModel loggedInUser)
+            {
+                _context = new ToDoDbContext();
+                _loggedInUser = loggedInUser;
+                _userRepository = new UserRepository(_context);
+                _categoryRepository = new CategoryRepository(_context);
+                EditCategoryCommand = new ViewModelCommand(ExecuteEditCategoryCommand);
+                DeleteCategoryCommand = new ViewModelCommand(ExecuteDeleteCategoryCommand);
+                SaveCategoryCommand = new ViewModelCommand(ExecuteSaveCategoryCommand, CanExecuteSaveCategoryCommand);
 
             LoadUserCategories();
-        }
-        private void LoadUserCategories()
-        {
-            string username = _loggedInUser.Username;
-            UserCategories = _userRepository.GetUserCategories(username);
-        }
-        private void ExecuteEditCategoryCommand(object obj)
-        {
-            if(obj is Category selectedCategory)
-            {
-                // Zapisz zmiany nazwy kategorii do bazy danych
-                _categoryRepository.UpdateCategory(selectedCategory);
-                _context.SaveChanges();
             }
-        }
+            private void LoadUserCategories()
+            {
+                string username = _loggedInUser.Username;
+                UserCategories = _userRepository.GetUserCategories(username);
+            }
+            private void ExecuteEditCategoryCommand(object obj)
+            {
+                if (obj is Category selectedCategory)
+                {
+                    SelectedCategory = selectedCategory;
+                }
+            }
 
-        private void ExecuteDeleteCategoryCommand(object obj)
-        {
-            // Logika usuwania kategorii
-        }
+            private void ExecuteSaveCategoryCommand(object obj)
+            {
+                if (SelectedCategory != null)
+                {
+                    // Zapisz zmiany nazwy kategorii do bazy danych
+                    _categoryRepository.UpdateCategory(SelectedCategory);
+                    _context.SaveChanges();
+                    SelectedCategory = null;
+                }
+            }
+
+            private bool CanExecuteSaveCategoryCommand(object obj)
+            {
+                return SelectedCategory != null;
+            }
+
+            private void ExecuteDeleteCategoryCommand(object obj)
+                {
+                    // Logika usuwania kategorii
+                }
+            }
     }
-}
