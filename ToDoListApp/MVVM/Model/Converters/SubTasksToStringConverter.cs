f5134f85e58a0a3ba70b6e6ef7f95243b2e2e78e
Aleksandra Niedźwiecka <olaniedz00@wp.pl>
2023-06-07 21:47:10 +0200

    SubTasks in Details

---
+++ ToDoListApp/MVVM/Model/Converters/SubTasksToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ToDoListApp.MVVM.Model.Converters
+{
+    public class SubTasksToStringConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is ICollection<Subtask> subTasks)
+            {
+                return string.Join(", ", subTasks.Select(c => c.Name));
+            }
+
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
